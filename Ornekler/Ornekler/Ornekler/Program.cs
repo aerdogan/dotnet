ab3ad63cb3958a959c0182171c9085c072c77dca
Ahmet Erdoğan <tuxposta@gmail.com>
2021-01-23 21:10:25 +0300

    Generic List Type Example

---
+++ Ornekler/Ornekler/Ornekler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ornekler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> liste = new MyList<string>();
+            liste.Add("Ahmet");
+            liste.Add("Deneme");
+            Console.WriteLine(liste.Length);
+
+            foreach (var isimler in liste.Items)
+            {
+                Console.WriteLine(isimler);
+            }
+        }
+    }
+}
