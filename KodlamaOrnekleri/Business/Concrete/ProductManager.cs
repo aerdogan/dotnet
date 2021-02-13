bb22ad84aa067d096854ae63534d3653674265b8
aerdogan <tuxposta@gmail.com>
2021-02-14 00:02:12 +0300

    WebAPI Added...

--- KodlamaOrnekleri/Business/Concrete/ProductManager.cs
+++ KodlamaOrnekleri/Business/Concrete/ProductManager.cs
@@ -30,10 +30,12 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==00)
+
+            if (DateTime.Now.Hour == 23)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
+
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
