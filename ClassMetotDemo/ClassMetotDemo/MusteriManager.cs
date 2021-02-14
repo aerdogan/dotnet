4ac62cb836024b6422d5fac0011b23cda0dd6432
aerdogan <tuxposta@gmail.com>
2021-02-14 03:26:42 +0300

    class example

---
+++ ClassMetotDemo/ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " Eklendi");
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine( musteri.Ad + " " + musteri.Soyad + " Silindi");
+        }
+        public void Update(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " Güncellendi");
+        }
+        public void List(Musteri[] musteriler)
+        {
+            for (int i = 0; i < musteriler.Length; i++)
+            {
+                Console.WriteLine("Ad Soyad :" + musteriler[i].Ad + " " + musteriler[i].Soyad + " TcKimlikNo :" + musteriler[i].TcKimlikNo + " " + "id:" + musteriler[i].Id);
+            }
+        }
+    }
+}
