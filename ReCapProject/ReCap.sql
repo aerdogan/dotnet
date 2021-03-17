9eefa3d39f2d6e0b7104b07ede6f69a68d7f03a0
Ahmet Erdoğan <tuxposta@gmail.com>
2021-03-17 17:03:52 +0300

    Update ReCap.sql

--- ReCapProject/ReCap.sql
+++ ReCapProject/ReCap.sql
@@ -1,6 +1,6 @@
 USE [ReCap]
 GO
-/****** Object:  Table [dbo].[Brands]    Script Date: 1.03.2021 00:40:42 ******/
+/****** Object:  Table [dbo].[Brands]    Script Date: 17.03.2021 17:02:25 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -14,7 +14,7 @@ CREATE TABLE [dbo].[Brands](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[CarImages]    Script Date: 1.03.2021 00:40:42 ******/
+/****** Object:  Table [dbo].[CarImages]    Script Date: 17.03.2021 17:02:25 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -30,7 +30,7 @@ CREATE TABLE [dbo].[CarImages](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Cars]    Script Date: 1.03.2021 00:40:42 ******/
+/****** Object:  Table [dbo].[Cars]    Script Date: 17.03.2021 17:02:25 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -42,14 +42,14 @@ CREATE TABLE [dbo].[Cars](
 	[ColorId] [int] NULL,
 	[ModelYear] [int] NULL,
 	[DailyPrice] [decimal](8, 2) NULL,
-	[Description] [nvarchar](50) NULL,
+	[Description] [nvarchar](250) NULL,
  CONSTRAINT [PK_Cars] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Colors]    Script Date: 1.03.2021 00:40:42 ******/
+/****** Object:  Table [dbo].[Colors]    Script Date: 17.03.2021 17:02:25 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -63,7 +63,7 @@ CREATE TABLE [dbo].[Colors](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Customers]    Script Date: 1.03.2021 00:40:42 ******/
+/****** Object:  Table [dbo].[Customers]    Script Date: 17.03.2021 17:02:25 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -78,7 +78,7 @@ CREATE TABLE [dbo].[Customers](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[OperationClaims]    Script Date: 1.03.2021 00:40:42 ******/
+/****** Object:  Table [dbo].[OperationClaims]    Script Date: 17.03.2021 17:02:25 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -92,7 +92,7 @@ CREATE TABLE [dbo].[OperationClaims](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Rentals]    Script Date: 1.03.2021 00:40:42 ******/
+/****** Object:  Table [dbo].[Rentals]    Script Date: 17.03.2021 17:02:25 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -109,7 +109,7 @@ CREATE TABLE [dbo].[Rentals](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[UserOperationClaims]    Script Date: 1.03.2021 00:40:42 ******/
+/****** Object:  Table [dbo].[UserOperationClaims]    Script Date: 17.03.2021 17:02:25 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -124,7 +124,7 @@ CREATE TABLE [dbo].[UserOperationClaims](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Users]    Script Date: 1.03.2021 00:40:42 ******/
+/****** Object:  Table [dbo].[Users]    Script Date: 17.03.2021 17:02:25 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -142,4 +142,69 @@ CREATE TABLE [dbo].[Users](
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[Brands] ON 
+
+INSERT [dbo].[Brands] ([Id], [Name]) VALUES (1, N'Opel')
+INSERT [dbo].[Brands] ([Id], [Name]) VALUES (4, N'Ford')
+INSERT [dbo].[Brands] ([Id], [Name]) VALUES (5, N'BMW')
+INSERT [dbo].[Brands] ([Id], [Name]) VALUES (6, N'Audi')
+SET IDENTITY_INSERT [dbo].[Brands] OFF
+GO
+SET IDENTITY_INSERT [dbo].[CarImages] ON 
+
+INSERT [dbo].[CarImages] ([Id], [CarId], [ImagePath], [Date]) VALUES (1, 6, N'8dffc603-3ac0-47b0-b194-a63edec64c0f.png', CAST(N'2021-03-17T14:22:15.807' AS DateTime))
+INSERT [dbo].[CarImages] ([Id], [CarId], [ImagePath], [Date]) VALUES (2, 6, N'0867d119-8a35-4e57-a352-a17be1ff2ac2.png', CAST(N'2021-03-17T14:22:25.593' AS DateTime))
+INSERT [dbo].[CarImages] ([Id], [CarId], [ImagePath], [Date]) VALUES (3, 6, N'ccb15c94-8267-4f30-89cb-3e1671e51bb2.png', CAST(N'2021-03-17T14:22:30.280' AS DateTime))
+INSERT [dbo].[CarImages] ([Id], [CarId], [ImagePath], [Date]) VALUES (4, 4, N'7d259b2c-0f23-4f9a-90ef-639c9869ace9.png', CAST(N'2021-03-17T14:29:14.050' AS DateTime))
+INSERT [dbo].[CarImages] ([Id], [CarId], [ImagePath], [Date]) VALUES (5, 1, N'b1d52525-f189-464b-9a61-a5a6ce0e9b5b.png', CAST(N'2021-03-17T14:29:36.443' AS DateTime))
+SET IDENTITY_INSERT [dbo].[CarImages] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Cars] ON 
+
+INSERT [dbo].[Cars] ([Id], [Name], [BrandId], [ColorId], [ModelYear], [DailyPrice], [Description]) VALUES (1, N'Yeni Opel Corsa', 1, 3, 2021, CAST(750.00 AS Decimal(8, 2)), N'Yeni Opel Corsa öncü teknolojileri, keyifli sürüşü ve performansı sunuyor.')
+INSERT [dbo].[Cars] ([Id], [Name], [BrandId], [ColorId], [ModelYear], [DailyPrice], [Description]) VALUES (4, N'Yeni Ford Focus', 4, 2, 2021, CAST(950.00 AS Decimal(8, 2)), N'Yeni Focus ergonomik tasarımı, şık ve geniş iç mekanı ve fonksiyonel detayları ile sizlere yüksek konforlu bir alan sunuyor.')
+INSERT [dbo].[Cars] ([Id], [Name], [BrandId], [ColorId], [ModelYear], [DailyPrice], [Description]) VALUES (6, N'Yeni BMW M5', 5, 5, 2021, CAST(1250.00 AS Decimal(8, 2)), N'Yeni BMW 5 Serisi Sedan M modelleri, tipik M sportifliğini, business sedanının konforu ve zarafetiyle olağanüstü bir şekilde bir araya getiriyor.')
+SET IDENTITY_INSERT [dbo].[Cars] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Colors] ON 
+
+INSERT [dbo].[Colors] ([Id], [Name]) VALUES (1, N'Siyah')
+INSERT [dbo].[Colors] ([Id], [Name]) VALUES (2, N'Beyaz')
+INSERT [dbo].[Colors] ([Id], [Name]) VALUES (3, N'Kırmızı')
+INSERT [dbo].[Colors] ([Id], [Name]) VALUES (4, N'Sarı')
+INSERT [dbo].[Colors] ([Id], [Name]) VALUES (5, N'Gri')
+SET IDENTITY_INSERT [dbo].[Colors] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Customers] ON 
+
+INSERT [dbo].[Customers] ([Id], [UserId], [CompanyName]) VALUES (1, 1, N'Müşteri')
+SET IDENTITY_INSERT [dbo].[Customers] OFF
+GO
+SET IDENTITY_INSERT [dbo].[OperationClaims] ON 
+
+INSERT [dbo].[OperationClaims] ([Id], [Name]) VALUES (1, N'car.add')
+INSERT [dbo].[OperationClaims] ([Id], [Name]) VALUES (2, N'carimage.add')
+SET IDENTITY_INSERT [dbo].[OperationClaims] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Rentals] ON 
+
+INSERT [dbo].[Rentals] ([Id], [CarId], [CustomerId], [RentDate], [ReturnDate]) VALUES (1, 1, 1, CAST(N'2021-02-16T02:08:40.147' AS DateTime), CAST(N'2021-02-16T02:08:40.317' AS DateTime))
+INSERT [dbo].[Rentals] ([Id], [CarId], [CustomerId], [RentDate], [ReturnDate]) VALUES (2, 1, 1, CAST(N'2021-02-16T02:08:40.330' AS DateTime), NULL)
+INSERT [dbo].[Rentals] ([Id], [CarId], [CustomerId], [RentDate], [ReturnDate]) VALUES (3, 6, 1, CAST(N'2021-02-26T17:54:46.860' AS DateTime), CAST(N'2021-02-26T17:54:46.957' AS DateTime))
+INSERT [dbo].[Rentals] ([Id], [CarId], [CustomerId], [RentDate], [ReturnDate]) VALUES (4, 6, 1, CAST(N'2021-02-26T17:54:46.963' AS DateTime), NULL)
+INSERT [dbo].[Rentals] ([Id], [CarId], [CustomerId], [RentDate], [ReturnDate]) VALUES (5, 4, 1, CAST(N'2021-02-28T19:28:32.157' AS DateTime), CAST(N'2021-02-28T19:28:32.240' AS DateTime))
+INSERT [dbo].[Rentals] ([Id], [CarId], [CustomerId], [RentDate], [ReturnDate]) VALUES (8, 4, 1, CAST(N'2021-03-03T00:00:00.000' AS DateTime), NULL)
+SET IDENTITY_INSERT [dbo].[Rentals] OFF
+GO
+SET IDENTITY_INSERT [dbo].[UserOperationClaims] ON 
+
+INSERT [dbo].[UserOperationClaims] ([Id], [UserId], [OperationClaimId]) VALUES (1, 1, 1)
+INSERT [dbo].[UserOperationClaims] ([Id], [UserId], [OperationClaimId]) VALUES (2, 1, 2)
+SET IDENTITY_INSERT [dbo].[UserOperationClaims] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Users] ON 
+
+INSERT [dbo].[Users] ([Id], [FirstName], [LastName], [Email], [PasswordHash], [PasswordSalt], [Status]) VALUES (1, N'ahmet', N'erdoğan', N'test@test.com', 0xBAB8895B428753B4D5CDB209E38DA8D953468F7C257E949B30B961CCD189141D6BCCE5858C741594C14FBA4CEED02EE02B0E03AE46DB9A1803D811124F96E2DF, 0x652EC3F2AA560A3C42DFEF9C16CC17651F70D16466AED802DF673B68AD70AF342B477D589CE9B2E99A586C00AB2E32EE755E42FC992A2B822F05AAD6522C699EA9C7492D024B1544AB7D21D37D3F35540DA35493D732272805F1F4697CAE5A3F417B761CC144C029F5113C64B3DB15EEEA7FC545D223FC097ECBD4459EC0C4A1, 1)
+SET IDENTITY_INSERT [dbo].[Users] OFF
 GO
