6d7eba9b81da412e1fb06ff070e6635c36b717c3
aerdogan <tuxposta@gmail.com>
2021-04-10 20:49:33 +0300

    refactor

--- ReCapProject/Business/Concrete/RentalManager.cs
+++ ReCapProject/Business/Concrete/RentalManager.cs
@@ -1,16 +1,13 @@
 ﻿using Business.Abstract;
-using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
-using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Business.Concrete
 {
@@ -64,23 +61,15 @@ namespace Business.Concrete
 
         public bool IsCarAvailable(int carId)
         {
-            using (ReCapContext context = new ReCapContext())
-            {
-                var result = from r in context.Rentals
-                             where r.CarId == carId && r.ReturnDate == null
-                             select r;
-                return (result.Count() == 0) ? true : false;
-            }
+            Rental result = _rentalDal.Get(r => r.CarId == carId && r.ReturnDate == null);
+            return result == null ? true : false;
         }
 
         public IResult CarIsReturned(int carId)
         {
-            using (ReCapContext context = new ReCapContext())
-            {
-                Rental result = _rentalDal.Get(r => r.CarId == carId && r.ReturnDate == null);
-                result.ReturnDate = DateTime.Now;
-                _rentalDal.Update(result);
-            }
+            Rental result = _rentalDal.Get(r => r.CarId == carId && r.ReturnDate == null);
+            result.ReturnDate = DateTime.Now;
+            _rentalDal.Update(result);
             return new SuccessResult(Messages.RentalUpdated);
         }
     }
