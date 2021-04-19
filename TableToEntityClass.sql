ddcd06e34154ce49e813a85e7d52679cf789779e
Ahmet Erdoğan <tuxposta@gmail.com>
2021-04-19 22:57:16 +0300

    Update TableToEntityClass.sql

--- TableToEntityClass.sql
+++ TableToEntityClass.sql
@@ -1,62 +1,62 @@
- -- TableName kısmından tablo ismini değiştirerek Class'larınızı oluşturabilirsiniz
- -- Tablolar(Varlıklar) çoğul olacağı için son karakterini temizleyecektir Örn : (Products tablosu için Product class'ı oluşacaktır) 
+-- MSSQL için aktif veritabanının C# Entity Class'larınızı oluşturur
  
-declare @TableName sysname = 'TableName'
-declare @Result varchar(max) = 'public class ' +LEFT(@TableName, LEN(@TableName) - 1)  + ':IEntity
-{'
+declare @tableName varchar(200)
+declare @columnName varchar(200)
+declare @datatype varchar(50)
+declare @sType varchar(50)
+declare @sProperty varchar(200)
 
-select @Result = @Result + '
-    public ' + ColumnType + NullableSign + ' ' + ColumnName + ' { get; set; }
-'
-from
-(
-    select 
-        replace(col.name, ' ', '_') ColumnName,
-        column_id ColumnId,
-        case typ.name 
-            when 'bigint' then 'long'
-            when 'binary' then 'byte[]'
-            when 'bit' then 'bool'
-            when 'char' then 'string'
-            when 'date' then 'DateTime'
-            when 'datetime' then 'DateTime'
-            when 'datetime2' then 'DateTime'
-            when 'datetimeoffset' then 'DateTimeOffset'
-            when 'decimal' then 'decimal'
-            when 'float' then 'double'
-            when 'image' then 'byte[]'
-            when 'int' then 'int'
-            when 'money' then 'decimal'
-            when 'nchar' then 'string'
-            when 'ntext' then 'string'
-            when 'numeric' then 'decimal'
-            when 'nvarchar' then 'string'
-            when 'real' then 'float'
-            when 'smalldatetime' then 'DateTime'
-            when 'smallint' then 'short'
-            when 'smallmoney' then 'decimal'
-            when 'text' then 'string'
-            when 'time' then 'TimeSpan'
-            when 'timestamp' then 'long'
-            when 'tinyint' then 'byte'
-            when 'uniqueidentifier' then 'Guid'
-            when 'varbinary' then 'byte[]'
-            when 'varchar' then 'string'
-            else 'UNKNOWN_' + typ.name
-        end ColumnType,
-        case 
-            when col.is_nullable = 1 and typ.name in ('bigint', 'bit', 'date', 'datetime', 'datetime2', 'datetimeoffset', 'decimal', 'float', 'int', 'money', 'numeric', 'real', 'smalldatetime', 'smallint', 'smallmoney', 'time', 'tinyint', 'uniqueidentifier') 
-            then '?' 
-            else '' 
-        end NullableSign
-    from sys.columns col
-        join sys.types typ on
-            col.system_type_id = typ.system_type_id AND col.user_type_id = typ.user_type_id
-    where object_id = object_id(@TableName)
-) t
-order by ColumnId
+DECLARE table_cursor CURSOR FOR 
+SELECT TABLE_NAME
+FROM [INFORMATION_SCHEMA].[TABLES]
 
-set @Result = @Result  + '
-}'
+OPEN table_cursor
 
-print @Result
+FETCH NEXT FROM table_cursor 
+INTO @tableName
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+
+PRINT 'public class ' + LEFT(@tableName, LEN(@tableName) - 1) + ' {'
+
+    DECLARE column_cursor CURSOR FOR 
+    SELECT COLUMN_NAME, DATA_TYPE
+    from [INFORMATION_SCHEMA].[COLUMNS] 
+	WHERE [TABLE_NAME] = @tableName
+	order by [ORDINAL_POSITION]
+
+    OPEN column_cursor
+    FETCH NEXT FROM column_cursor INTO @columnName, @datatype
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+
+	-- datatype
+	select @sType = case @datatype
+	when 'int' then 'Int32'
+	when 'decimal' then 'Decimal'
+	when 'money' then 'Decimal'
+	when 'char' then 'String'
+	when 'nchar' then 'String'
+	when 'varchar' then 'String'
+	when 'nvarchar' then 'String'
+	when 'uniqueidentifier' then 'Guid'
+	when 'datetime' then 'DateTime'
+	when 'bit' then 'Boolean'
+	else 'String'
+	END
+		SELECT @sProperty = '	public ' + @sType + ' ' + @columnName + ' { get; set;}'
+		PRINT @sProperty
+		FETCH NEXT FROM column_cursor INTO @columnName, @datatype
+	END
+    CLOSE column_cursor
+    DEALLOCATE column_cursor
+
+	print '}'
+	print ''
+    FETCH NEXT FROM table_cursor 
+    INTO @tableName
+END
+CLOSE table_cursor
+DEALLOCATE table_cursor
