2d9efc75b0eff76cdd5b0df527db0532b0be34d8
aerdogan <tuxposta@gmail.com>
2021-05-16 14:18:28 +0300

    moved to new repo

--- KodlamaOrnekleri/Core/Utilities/Messages/AspectMessages.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Core.Utilities.Messages
-{
-    public static class AspectMessages
-    {
-        public static string WrongValidationType = "Wrong Validation Type";
-        public static string WrongLoggerType = "Wrong Logger Type";
-    }
-}
